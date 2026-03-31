c6272af15c9294e401c1751b8c3bac4262c04a76
Cnnb01 <nyamambanyaguch@gmail.com>
2026-03-31 12:16:08 +0300

    created users models and apis end to end

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        MANAGER = "manager", "Manager"
+        AGENT = "agent", "Agent"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.AGENT,
+    )

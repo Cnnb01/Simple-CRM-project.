cad73e5d209654a90fc62c3bbe33d029d800e1a3
Cnnb01 <nyamambanyaguch@gmail.com>
2026-04-23 22:01:09 +0300

    setting up cors

--- crm-backend/mycrmsite/settings.py
+++ crm-backend/mycrmsite/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
     'django_celery_beat',
     'leads',
     'users',
+    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -151,4 +153,11 @@ CELERY_BEAT_SCHEDULE = {
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 
